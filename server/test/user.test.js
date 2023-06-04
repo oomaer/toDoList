5253cfa41e896e389b95fecfc0ee235623425be8
umair <cyumair@hotmail.com>
2023-06-04 13:50:12 +0500

    add todo test

--- server/test/user.test.js
+++ server/test/user.test.js
@@ -127,7 +127,17 @@ describe('User', () => {
 describe('Todo', () => {
 
     describe('POST /todo', () => {
-        
+
+        describe('when jwt is not provided', () => {
+            it('should return 401', async () => {
+                const response = await supertest(app).post('/todo').send({
+                    description: 'This is the todo task',
+                    completed: false,
+                })
+                expect(response.statusCode).toBe(401)
+            })
+        })
+
     })
 
 })
